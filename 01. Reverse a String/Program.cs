f5570160fbf72e8a8313afd8ac2880e1fda4e1ed
Angel Tabakov <atabakov99@abv.bg>
2022-05-16 21:27:37 +0300

    Add project files.

---
+++ 01. Reverse a String/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class StringReversal
+{
+    static void Main()
+    {
+        string str = Console.ReadLine();
+
+        Stack<char> stack = new Stack<char>();
+        foreach (var ch in str)
+        {
+            stack.Push(ch);
+        }
+        while (stack.Count > 0)
+        {
+            Console.Write(stack.Pop());
+        }
+    }
+}
+
