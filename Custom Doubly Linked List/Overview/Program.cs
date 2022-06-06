8efec3793b206fc013e12b7e44e8d0dff7153ebb
ANGEL <atabakov99@abv.bg>
2022-06-06 20:54:57 +0300

    Implementing Linked List

---
+++ Custom Doubly Linked List/Overview/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Overview
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var linkedList = new DoublyLinkedList();
+
+            linkedList.AddHead(1);
+            linkedList.AddHead(2);
+            linkedList.AddHead(3);
+            linkedList.AddHead(4);
+
+            Console.WriteLine($"Head: {linkedList.Head.Value}");
+            Console.WriteLine($"Tail: {linkedList.Tail.Value}");
+
+            Console.WriteLine($"Next to head: {linkedList.Head.Next.Value}");
+            Console.WriteLine($"Previous to tail: {linkedList.Tail.Previous.Value}");
+
+        }
+    }
+}
