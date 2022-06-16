afe63413b8c3689879cfb5402eaaf9440b607a0f
ANGEL <atabakov99@abv.bg>
2022-06-16 14:16:17 +0300

    Iterators and Comparator - Exercise

---
+++ Iterators and Comparators - Exercise/04. Froggy/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace T04.Froggy
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var lake = new Lake(Console.ReadLine().Split(", ").Select(int.Parse).ToArray());
+            Console.WriteLine(String.Join(", ", lake));
+        }
+    }
+}
