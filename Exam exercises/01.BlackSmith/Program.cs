d6c113f90cfbcc954f7a896b79e326b33263d95e
ANGEL <atabakov99@abv.bg>
2022-06-30 20:24:15 +0300

    Added files.

---
+++ Exam exercises/01.BlackSmith/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.BlackSmith
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> inputSteel = new Queue<int>(Console.ReadLine().Split(' ').Select(int.Parse).ToArray());
+            Stack<int> inputCarbon = new Stack<int>(Console.ReadLine().Split(' ').Select(int.Parse).ToArray());
+            Dictionary<string, int> swords = new Dictionary<string, int>();
+            swords.Add("Gladius", 0);
+            swords.Add("Shamshir", 0);
+            swords.Add("Katana", 0);
+            swords.Add("Sabre", 0);
+            swords.Add("Broadsword", 0);
+            int swordsAdded = 0;
+
+
+
+
+            while (inputSteel.Count != 0 && inputCarbon.Count != 0)
+            {
+            int currentSteel = inputSteel.Peek();
+
+            int currentCarbon = inputCarbon.Peek();
+
+            int sum = currentSteel + currentCarbon;
+
+                if (sum == 70)
+                {
+                    swords["Gladius"]++;
+                    inputSteel.Dequeue();
+                    inputCarbon.Pop();
+                    swordsAdded++;
+                }
+                else if (sum == 80)
+                {
+                    swords["Shamshir"]++;
+                    inputSteel.Dequeue();
+                    inputCarbon.Pop();
+                    swordsAdded++;
+                }
+                else if (sum == 90)
+                {
+                    swords["Katana"]++;
+                    inputSteel.Dequeue();
+                    inputCarbon.Pop();
+                    swordsAdded++;
+                }
+                else if (sum == 110)
+                {
+                    swords["Sabre"]++;
+                    inputSteel.Dequeue();
+                    inputCarbon.Pop();
+                    swordsAdded++;
+                }
+                else if (sum == 150)
+                {
+                    swords["Broadsword"]++;
+                    inputSteel.Dequeue();
+                    inputCarbon.Pop();
+                    swordsAdded++;
+                }
+                else
+                {
+                    inputSteel.Dequeue();
+                    
+                    inputCarbon.Push(inputCarbon.Pop() + 5);
+                }
+            }
+            if (swordsAdded > 0)
+            {
+                Console.WriteLine($"You have forged {swordsAdded} swords.");
+            }
+            else
+            {
+                Console.WriteLine("You did not have enough resources to forge a sword.");
+            }
+            if (inputSteel.Count == 0)
+            {
+                Console.WriteLine("Steel left: none");
+            }
+            else
+            {
+                Console.WriteLine($"Steel left: {String.Join(", ", inputSteel)}");
+
+            }
+            if (inputCarbon.Count == 0)
+            {
+                Console.WriteLine("Carbon left: none");
+            }
+            else
+            {
+                Console.WriteLine($"Carbon left: {String.Join(", ", inputCarbon)}");
+            }
+
+            foreach (var sword in swords.OrderBy(x => x.Key))
+            {
+                if (sword.Value > 0)
+                {
+                    string nameSword = sword.Key;
+                    int valueSword = sword.Value;
+                    Console.WriteLine($"{nameSword}: {valueSword}");
+                }
+            }
+            
+
+        }
+    }
+}
---
+++ TestAdvancedModule/Second/Program.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02_Beaver_at_Work
+{
+    public class Program
+    {
+        private static int playerRow;
+        private static int playerCol;
+        private static char[,] matrix;
+        private static string lastDirection;
+        private static int countOfHoles = 0;
+        private static int countOfRods = 0;
+
+
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+            matrix = new char[n, n];
+
+            //Initialize Matrix and set bunny position
+            for (int i = 0; i < n; i++)
+            {
+                char[] input = Console.ReadLine()
+                    .ToCharArray();
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    matrix[i, j] = input[j];
+
+                    if (matrix[i, j] == 'V')
+                    {
+                        playerRow = i;
+                        playerCol = j;
+                    }
+                  
+                    
+                }
+            }
+
+            string direction = Console.ReadLine();
+
+            while (direction != "End")
+            {
+                lastDirection = direction;
+
+                if (direction == "up")
+                {
+                    Move(-1, 0);
+                }
+                else if (direction == "down")
+                {
+                    Move(1, 0);
+                }
+                else if (direction == "right")
+                {
+                    Move(0, 1);
+                }
+                else if (direction == "left")
+                {
+                    Move(0, -1);
+                }
+
+                direction = Console.ReadLine();
+            }
+
+
+            if (true)
+            {
+
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write((char)matrix[i, j]);
+                    Console.Write(' ');
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void Move(int row, int col)
+        {
+            if (!IsInside(playerRow + row, playerCol + col))
+            {
+                return;
+            }
+            matrix[playerRow, playerCol] = '*';
+            playerRow += row;
+            playerCol += col;
+
+            if (char.IsLetter(matrix[playerRow, playerCol]))
+            {
+
+                Console.WriteLine();
+         
+            }
+            else if (matrix[playerRow, playerCol] == 'R')
+            {
+                matrix[playerRow, playerCol] = '-';
+
+                if (lastDirection == "up")
+                {
+                    if (playerRow == 0)
+                    {
+                        if (char.IsDigit(matrix[matrix.GetLength(0) - 1, playerCol]))
+                        {
+                            countOfHoles++;
+                        }
+                        playerRow = matrix.GetLength(0) - 1;
+                        matrix[playerRow, playerCol] = 'V';
+                    }
+                    else
+                    {
+                        if (char.IsDigit(matrix[0, playerCol]))
+                        {
+                            countOfHoles++;
+                        }
+                        playerRow = 0;
+                        matrix[playerRow, playerCol] = 'V';
+                    }
+                }
+                else if (lastDirection == "down")
+                {
+                    if (playerRow == matrix.GetLength(0) - 1)
+                    {
+                        if (char.IsLetter(matrix[0, playerCol]))
+                        {
+                            Console.WriteLine("Vanko hit a rod!");
+                            countOfRods++;
+                        }
+                        playerRow = 0;
+                        matrix[playerRow, playerCol] = 'V';
+                    }
+                    else
+                    {
+                        
+                        playerRow = matrix.GetLength(0) - 1;
+                        matrix[playerRow, playerCol] = 'V';
+                    }
+                }
+                else if (lastDirection == "right")
+                {
+                    if (playerCol == matrix.GetLength(1) - 1)
+                    {
+                        
+                        playerCol = 0;
+                        matrix[playerRow, playerCol] = 'V';
+                    }
+                    else
+                    {
+                        if (char.IsLetter(matrix[playerRow, matrix.GetLength(1) - 1]))
+                        {
+                            Console.WriteLine("Vanko hit a rod!");
+                            countOfRods++;
+                        }
+                        playerCol = matrix.GetLength(1) - 1;
+                        matrix[playerRow, playerCol] = 'V';
+                    }
+                }
+                else if (lastDirection == "left")
+                {
+                    if (playerCol == 0)
+                    {
+                        if (char.IsLetter(matrix[playerRow, matrix.GetLength(1) - 1]))
+                        {
+                            Console.WriteLine("Vanko hit a rod!");
+                            countOfRods++;
+                        }
+                        playerCol = matrix.GetLength(1) - 1;
+                        matrix[playerRow, playerCol] = 'V';
+                    }
+                    else
+                    {
+                        if (char.IsLetter(matrix[playerRow, 0]))
+                        {
+                            Console.WriteLine("Vanko hit a rod!");
+                            countOfRods++;
+                        }
+                        playerCol = 0;
+                        matrix[playerRow, playerCol] = 'V';
+                    }
+                }
+            }
+            else
+            {
+                matrix[playerRow, playerCol] = 'V';
+            }
+        }
+
+        private static bool IsInside(int row, int col)
+        {
+            return row >= 0 && row < matrix.GetLength(0) &&
+                   col >= 0 && col < matrix.GetLength(1);
+        }
+    }
+}
